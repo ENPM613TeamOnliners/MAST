e295adb1b5725573d0c4020b5fe19598db4be53b
Mireya Baur <miarredondo3@gmail.com>
2019-11-03 21:31:52 -0500

    changes to mast

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon  @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Modern Academic Security Training (MAST)</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn>
+         <router-link to="/logout" exact>Logout</router-link>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card>
+    <v-tabs center-active dark>
+      <v-tab>Home</v-tab>
+      <v-tab>Courses</v-tab>
+      <v-tab>Messages</v-tab>
+      <v-tab>Help</v-tab>
+      <v-tab>About</v-tab>
+
+      <!-- Tab content -->
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+           Home
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            Courses
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            Messages
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            Help
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+          
+        <v-card flat>
+          <v-card-text>
+            <About />
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+import About from "@/views/About.vue";
+export default {
+  name: "Landing",
+  components: {
+    About
+  }
+};
+</script>
