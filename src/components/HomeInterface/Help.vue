dcfa24aeb07299b063b24b5f6eb99cfaad25a587
Mireya Baur <miarredondo3@gmail.com>
2019-12-01 17:19:17 -0500

    test change

--- src/components/HomeInterface/Help.vue
+++ src/components/HomeInterface/Help.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <h3 class="text-uppercase">This is the help page!</h3>
+    <h3 class="text-uppercase">This is the help page view!</h3>
   </v-container>
 </template>
 
