264b4c6a185ecc52716a8c9773c9cdd149e6f8a9
Mireya Baur <miarredondo3@gmail.com>
2019-12-01 00:27:48 -0500

    Linting and provided missing name

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,17 +1,16 @@
 <template>
-    <div class="alert alert-danger">
-        {{message}}
-    </div>
+  <div class="alert alert-danger">{{message}}</div>
 </template>
 <script>
 export default {
-    data: function(){
-        return {
-            message: ""
-        }
-    },
-    mounted(){
-        this.message = this.$route.query.message;
-    }
-}
+  name: "Error",
+  data: function() {
+    return {
+      message: ""
+    };
+  },
+  mounted() {
+    this.message = this.$route.query.message;
+  }
+};
 </script>
--- src/components/LogoutSuccess.vue
+++ src/components/LogoutSuccess.vue
@@ -1,15 +1,14 @@
 <template>
-<div class="row">
+  <div class="row">
     <div class="col text-center">
-        <h2>Logged Out Successfully</h2>
-        <router-link to="/login">Login</router-link>
+      <h2>Logged Out Successfully</h2>
+      <router-link to="/login">Login</router-link>
     </div>
-</div>
+  </div>
 </template>
 <script>
 export default {
-    mounted: function(){
-        
-    }
-}
+  name: "LogoutSuccess",
+  mounted: function() {}
+};
 </script>
