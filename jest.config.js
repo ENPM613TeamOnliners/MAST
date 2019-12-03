c3b50f416d974d252d0eecab0d1c29e0feeea4c9
Mireya Baur <miarredondo3@gmail.com>
2019-12-03 00:31:02 -0500

    unit test

--- jest.config.js
+++ jest.config.js
@@ -13,11 +13,20 @@ module.exports = {
     'jsx',
     'vue',
   ],
-
+  moduleDirectories: [
+    'node_modules',
+    'src',
+  ],
+  // roots: [
+  //   '<rootDir>',
+  // ],
   // eslint-disable-next-line max-len
   // A map from regular expressions to module names that allow to stub out resources with a single module
+  // moduleNameMapper: {
+  //   '^@/(.*)$': '<rootDir>/src/$1',
+  // },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$l',
   },
 
   snapshotSerializers: [
@@ -26,24 +35,21 @@ module.exports = {
 
 
   // The glob patterns Jest uses to detect test files
+  // testMatch: [
+  //   '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/tests/unit/**/*.(js|jsx|ts|tsx)',
+  // ],
   testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/tests/unit/**/*.(js|jsx|ts|tsx)',
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)',
   ],
 
-
-  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-  testURL: 'http://localhost/',
-
-  // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
-  // timers: "real",
-
+  testURL: 'http://localhost',
   // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.(vue)$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|wpff2)$': 'jest-transform-stub',
     '^.+\\jsx?$': 'babel-jest',
   },
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
     '/node_modules/',
     '<rootDir>/node_modules/(?!vuetify)',
