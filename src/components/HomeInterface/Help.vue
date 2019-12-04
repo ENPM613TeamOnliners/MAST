d5b12c0d6c8ced01a0bf4d6fae4fdfca21324e4f
Mireya Baur <miarredondo3@gmail.com>
2019-12-04 18:58:14 -0500

    Help desk information

--- src/components/HomeInterface/Help.vue
+++ src/components/HomeInterface/Help.vue
@@ -2,11 +2,29 @@
   <v-container>
     <h1 class="text-uppercase">MAST Documentation</h1>
     <br>
-    <a href="">How to Enroll in a course.</a>
+    <b>How to Enroll in a course.</b>
+    <ol>
+      <li>Create an account on MAST by login in.</li>
+      <li>Go to <a href="/courses">Courses</a> tab.</li>
+      <li>Click Course Syllabus.</li>
+      <li>Click enroll.</li>
+    </ol>
     <br>
-    <a href="">How to view a course.</a>
+    <b>How to view a course.</b>
+    <ol>
+      <li>Sign in to your MAST account. Note: you cannot view courses without a MAST account.</li>
+      <li>Select <a href="/courses">Courses</a> tab.</li>
+      <li>Now you are able to view courses.</li>
+    </ol>
     <br>
-    <a href="">How to view your account.</a>
+    <b>How to view your account.</b>
+    <ol> 
+      <li>Sign in to your MAST account.</li>
+      <li>Click <v-icon>mdi-menu</v-icon></li>
+      <li>Select <b>My Account.</b></li>
+    </ol>
+    <br>
+    <b>For any other comments, questions, or concerns, please contact your MAST helpdesk.</b>
   </v-container>
 </template>
 
